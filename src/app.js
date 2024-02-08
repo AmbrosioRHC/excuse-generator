d392fb9a5700b2f072c4aba57e52c318747f388f
AmbrosioRHC <ambrosio.rhc@gmail.com>
2024-02-08 00:42:12 +0000

    generador beta

--- src/app.js
+++ src/app.js
@@ -7,5 +7,13 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let objeto = ["", "el llavero", "la tarea", "el computador"];
+  let culpable = ["", "perro", "amigo", "hermano"];
+  let accion = ["", " se comió", "rompió", "perdió"];
+
+  let culpableAleatorio = culpable[Math.floor(Math.random() * 3 + 1)];
+  let objetoAleatorio = objeto[Math.floor(Math.random() * 3 + 1)];
+  let accionAleatoria = accion[Math.floor(Math.random() * 3 + 1)];
+
+  console.log(`Mi ${culpableAleatorio} ${accionAleatoria} ${objetoAleatorio}`);
 };
